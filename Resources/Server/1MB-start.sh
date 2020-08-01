7bd053025efe1167e20661c29a076105a9c7a4fc
mrfloris <mrfloris@gmail.com>
2020-08-01 12:31:24 +0200

    Updated MC start script to version 2.0 - updated header - bumped version number - removed all the code to run it without forking - switched to new method of running - removed stupid NOGO checker - merged conditionals - requires 1MB-Minecraft.sh now to run AT ALL - will check if ^ script is there - will check if ^ script is executable - script can now be used for both spigot/paper engines - moved config things around - made it way easier to configure - removed hardcoded theme - added my regular theme colors - script quits earlier when it's sudo or root user - echo moved to output function - merged or used some code from version 2.0 of buildtools script - removed obsolete code stuff to the max, configure it in Minecraft script instead - slowly making screen session name more dynamic - renamed ^ that to _serverName - tested it on macOS Catalina - did not test it on Ubuntu 20 yet

--- Resources/Server/1MB-start.sh
+++ Resources/Server/1MB-start.sh
@@ -1,134 +1,60 @@
 #!/bin/bash
 
 # @Filename: 1MB-start.sh
-# @Version: 1.5, build 025 for Spigot 1.16.1 (java 11)
-# @Release: July 25th, 2020
-# @Description: Helps us start a Minecraft Spigot 1.16.1 server.
+# @Version: 2.0, build 026 for Spigot 1.16.1 (java 11)
+# @Release: August 1st, 2020
+# @Description: Helps us start and fork a Minecraft Spigot 1.16.1 server.
 # @Contact:	I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: floris#0233 on https://discord.gg/KzTDhxv
 # @Install: chmod a+x 1MB-start.sh
 # @Syntax: ./1MB-start.sh
-
-# @Installation:
-# Type: chmod a+x 1MB-start.sh    and    chmod a+x 1MB-minecraft.sh
-# Type: nano -w 1MB-start.sh      and    nano -w 1MB-minecraft.sh
-# And edit the configuration part of both files.
-
-# @Usage:
-# To start the Minecraft Spigot Server each time, 
-# and fork it into the background, Type:  ./1MB-start.sh
-# To go into the server console, type: screen -r
-# if you have more than 1 screen session running, screen -ls, it's the
-# one with .spigot, behind the pid id. then: screen -r spigot
-# Tip: To get out of the server console, without quitting: Control+AD
-
-# @URL: Latest source, info, & terms: https://scripts.1moreblock.com/
-
+# @URL: Latest source, info, & support: https://scripts.1moreblock.com/
 
 ### CONFIGURATION
 #
 # Stuff here you COULD change, but ONLY if you really really have to
-# Note please that if you can fork, 1MB-minecraft.sh's values will override
 #
 ###
 
-# Which version are we running?
-MINECRAFT_VERSION="1.16.1"
+#todo take input for servername $1 to set to _serverName
+_serverName="mcserver"
+# this name makes it easier to spot in screen -ls
+# Keep this lowercase, short, simple, no weird characters
 
-JAVA_VERSION="11.0"
-#	11.0 = java 11, can be used for Minecraft 1.13.x and up.
-#	1.8 = java 8, required for Minecraft 1.12.x and up.
-
-JAVA_MEMORY="-Xms10G -Xmx10G"
-#	"" = uses the default
-#	"-Xmx2G" = maximum memory allocation pool of memory for JVM.
-#	"-Xms1G" = initial memory allocation pool of memory for JVM.
-# For Spigot servers we recommend -Xms10G -Xmx10G for 16GB systems.
-# More details here: https://stackoverflow.com/questions/14763079/
-
-JAR_PARAMS="-Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true"
-# 	-Dhttps.protocols=TLSv1 (temp fix for discordsrv)
-#	-Dcom.mojang.eula.agree=true (Mojang EULA agreement)
-#	-Dfile.encoding=UTF-8 (ensure that all UTF-8 chars are being saved properly)
-#	-Dapple.awt.UIElement=true (helps on macOS to not show icon in cmd-tab)
-
-JAR_UPGRADE=""
-# 	!! Use only if you know you need this !!
-#	Leave empty! (for every day running)
-# 	--forceUpgrade (one time convert chunks to new engine)
-# 	--eraseCache (lightning fix)
-
-SCREEN_NAME="spigotlive"
-# 	spigot (makes it easier to spot in screen -ls)
-#	If you want each server to have a unique identifier,
-# 	then this the place to set one for each server.
-
+_debug=true
+# set to false if you want as little visual output as possible.
 
 #######################################################
 #### !You're done, stop editing beyond this point! ####
 #######################################################
 
-
-MCJAR="spigot-$MINECRAFT_VERSION.jar"
-NOGUI="--nogui" # leave "" if you want the 1.15.2 server-gui
-LETSGO="no"
-
-if [ "$JAVA_VERSION" != "11.0" ]; then
-	# 08 (if you wish to run spigot 1.12.2 or 1.13.2)
-	JAVA_JDK="/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/java"
-else
-	# 11 (if you wish to run spigot for 1.13.2, 1.14.2, or 1.15.2)
-	JAVA_JDK="/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java"
-fi
-
-# Below is your java start up line for Spigot, edit accordingly.
-JAVACMD="$JAVA_JDK $JAVA_MEMORY $JAR_PARAMS -jar $MCJAR $JAR_UPGRADE $NOGUI"
-
 [ "$EUID" -eq 0 ] && echo -e "\n***!!*** This script should not be run using sudo, or as the root user!\n" && exit 1
+Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
+_sibling="1MB-minecraft.sh"
 
-if type "tput" > /dev/null 2>&1 ; then tput setaf 3 ; fi
-echo -e "\n* Attempting to start your Spigot $SCREEN_NAME server ... "
-if type "tput" > /dev/null 2>&1 ; then tput sgr0 ; fi
+function _output {
+	_args="${*:1}"
+	_prefix="(Debug)"
+	[[ "$_debug" == true ]] && echo -e "\\n$Y$_prefix$R$C $_args $R"
+}
 
-if type "screen" > /dev/null 2>&1; then
-	LETSGO="yes"
-	if [ -f "$MCJAR" ]; then
-		LETSGO="yes"
-	else
-		LETSGO="no"
-		echo -e "\n >> Error: '$MCJAR' not found. (Can't load your Spigot Server now, bye)"
-		exit
-	fi
-	
-else
-	LETSGO="no"
-	echo -e "\n >> Error: 'screen' not found."
-	
-	if [ -f "$MCJAR" ]; then
-		# we have the jar, but not screen, we can load mc, 
-		# just not fork to bg. so lets set it to no anyway
-		LETSGO="no"
-	else
-		LETSGO="no"
-		echo -e "\n >> Error: '$MCJAR' not found. (Can't load your Spigot Server now, bye)"
-		exit
-	fi
-	
+if [ ! -x "$_sibling" ]; then
+ 	_output "This '$0' script requires '$_sibling' to work and be executable. Correct the permissions, or download it from https://scripts.1moreblock.com/ "
+ 	exit 1
 fi
 
-if [[ "$LETSGO" == "yes" ]]; then
-	if type "tput" > /dev/null 2>&1 ; then tput setaf 3 ; fi
-	echo -e "\n* Forking into background."
-	if type "tput" > /dev/null 2>&1 ; then tput sgr0 ; fi
-	screen -dmS $SCREEN_NAME bash 1MB-minecraft.sh
-	screen -ls
+_output "Attempting to start your Minecraft '$_serverName' server ... "
+
+if type "screen" >/dev/null 2>&1; then
+	_output "Found 'screen', attempting to fork session into background ..."
+	screen -dmS $_serverName bash "$_sibling"
+	[[ "$_debug" == true ]] && screen -ls
 else
-	echo -e "\n >> Can't fork into background, starting your Spigot Server normally ..."
-	$JAVACMD
+	_output "Oops, 'screen' not installed. Can't fork session, trying '$_sibling' without ..."
+	sleep 3
+	bash "$_sibling" || _output "Something is wrong, I could not start your server."
 fi
 
-if type "tput" > /dev/null 2>&1 ; then tput setaf 3 ; fi
-echo -e "* Done."
-if type "tput" > /dev/null 2>&1 ; then tput sgr0 ; fi
+_output "Done."
 
 #EOF (c)2011-2020 Floris Fiedeldij Dop

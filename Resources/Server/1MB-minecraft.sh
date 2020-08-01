75c24c0b4967fab2e7f6347506998d55e3f43e44
mrfloris <mrfloris@gmail.com>
2020-08-01 12:28:40 +0200

    Updated Minecraft script to version 2.0 - updated header - bumped version number - introduced engine toggler - added ability to override engine name - prefers paper over spigot engine if found - if paper isn't found and spigot is there, runs spigot - script can now be used for both spigot/paper engines - moved config things around - made it a bit easier to configure - removed hardcoded theme - added my regular theme colors - script quits earlier when it's sudo or root user - echo moved to output function - merged or used some code from version 2.0 of buildtools script - initial engine toggler used, 'it works', but logic will be improved - supports nogui toggler for 1.16 servers - removed obsolete code stuff probably - slowly making screen session name more dynamic - renamed ^ that to _serverName - tested it on macOS Catalina - did not test it on ubuntu 20 yet

--- Resources/Server/1MB-minecraft.sh
+++ Resources/Server/1MB-minecraft.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
 # @Filename: 1MB-minecraft.sh
-# @Version: 1.5, build 025 for Spigot 1.16.1 (java 11)
-# @Release: July 25th, 2020
+# @Version: 2.0, build 026 for Spigot 1.16.1 (java 11)
+# @Release: August 1st, 2020
 # @Description: Helps us start a Minecraft Spigot 1.16.1 server.
 # @Contact:	I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: floris#0233 on https://discord.gg/KzTDhxv
-# @Install: chmod a+x 1MB-start.sh
-# @Syntax: ./1MB-start.sh
-
-# @Installation:
-# Type: chmod a+x 1MB-start.sh    and    chmod a+x 1MB-minecraft.sh
-# Type: nano -w 1MB-start.sh      and    nano -w 1MB-minecraft.sh
-# And edit the configuration part of both files.
-
-# @Usage:
-# To start the Minecraft Spigot Server each time, 
-# and NOT fork it into the background, Type:  ./1MB-minecraft.sh
-
-# @URL: Latest source, info, & terms: https://scripts.1moreblock.com/
+# @Install: chmod a+x 1MB-minecraft.sh
+# @Syntax: ./1MB-minecraft.sh
+# @URL: Latest source, info, & support: https://scripts.1moreblock.com/
 
 
 ### CONFIGURATION
 #
 # Stuff here you COULD change, but ONLY if you really really have to
-# Note please that if you can fork, 1MB-minecraft.sh's values will override
 #
 ###
 
 # Which version are we running?
 MINECRAFT_VERSION="1.16.1"
+
+# Override auto engine jar detection; only use this if you have issues
+_engine=""
+# "" assumes auto detection for <engine>-1.16.1.jar 
+# "spigot" assumes to look for spigot-1.16.1.jar
+# "paper" assumes to look for paper-1.16.1.jar
 
 JAVA_VERSION="11.0"
 #	11.0 = java 11, can be used for Minecraft 1.13.x and up.
@@ -52,35 +47,66 @@ JAR_UPGRADE=""
 # 	!! Use only if you know you need this !!
 #	Leave empty! (for every day running)
 # 	--forceUpgrade (one time convert chunks to new engine)
-# 	--eraseCache (lightning fix)
-
-SCREEN_NAME="spigotlive"
-# 	spigot (makes it easier to spot in screen -ls)
-#	If you want each server to have a unique identifier,
-# 	then this the place to set one for each server.
-# 	Examples: dev test beta live surivval creative s01 s02
-
+# 	--eraseCache (removing caches, lightning fix)
 
 #######################################################
 #### !You're done, stop editing beyond this point! ####
 #######################################################
 
+[ "$EUID" -eq 0 ] && echo -e "\n***!!*** This script should not be run using sudo, or as the root user!\n" && exit 1
+Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
 
-MCJAR="spigot-$MINECRAFT_VERSION.jar"
-NOGUI="--nogui" # leave "" if you want the 1.15.2 server-gui
+function _output {
+	_args="${*:1}"
+	_prefix="(Debug)"
+	[[ "$_debug" == true ]] && echo -e "\\n$Y$_prefix$R$C $_args $R"
+}
+
+# before we continue, let's figure out if we have spigot or paper
+if [ -n "$_engine" ];
+then
+	# It's not empty, let's use what they have and try to find that jar.
+	_enginejar="$_engine-$MINECRAFT_VERSION.jar"
+	if [[ -f "$_enginejar" ]]; then
+		# we found it, set it and continue the script. todo: we can simplify this logic
+		MCJAR="$_enginejar"
+	else
+		# we did not find it, time to complain and bail out.
+		_output "Oops, we did not find $_enginejar, please check your configuration."
+		exit 1
+	fi
+else
+	# it's empty - let's figure out if there's a spigot or paper jar
+	_enginejar="paper-$MINECRAFT_VERSION.jar"
+	if [[ -f "$_enginejar" ]]; then
+		# we found paper, set it and continue the script. 
+		MCJAR="$_enginejar"
+	else
+		# we did not find paper, but maybe there's a spigot jar
+		_enginejar="spigot-$MINECRAFT_VERSION.jar"
+		if [[ -f "$_enginejar" ]]; then
+			# found spigot, set it and continue the script.
+			MCJAR="$_enginejar"
+		else
+			# we did not find paper, and now also not spigot, time to bail out.
+			_output "Oops, we did not find a paper or $_enginejar, please read a manual."
+			exit 1
+		fi
+	fi
+fi
+
+NOGUI="--nogui" # leave "" if you want the 1.16.1 server-gui
 
 if [ "$JAVA_VERSION" != "11.0" ]; then
 	# 08 (if you wish to run spigot 1.12.2 or 1.13.2)
 	JAVA_JDK="/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/java"
 else
-	# 11 (if you wish to run spigot for 1.13.2, 1.14.2, or 1.15.2)
+	# 11 (if you wish to run spigot for 1.13.2 - 1.16.1)
 	JAVA_JDK="/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java"
 fi
 
 # Below is your java start up line for Spigot, edit accordingly.
 JAVACMD="$JAVA_JDK $JAVA_MEMORY $JAR_PARAMS -jar $MCJAR $JAR_UPGRADE $NOGUI"
-
-[ "$EUID" -eq 0 ] && echo -e "\n***!!*** This script should not be run using sudo, or as the root user!\n" && exit 1
 
 $JAVACMD
 

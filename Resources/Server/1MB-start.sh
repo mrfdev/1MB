975e3d714578a04614b188c067bfcde186bb7b9c
mrfloris <mrfloris@gmail.com>
2020-08-02 14:33:37 +0200

    Bigger update to v2 - Cleaned up source code - Condensed source code where logical - Small version bump - Updated header text slightly - The 'you can edit this' portion is basically nonexistent now - Configurations that should almost never be touched has been pushed down - The _outout function has been expanded. - Sibling and Privileged tests use theme now, and _output - Checking for user input (only first variable) - ./1MB-start.sh <word> can be a short, unique, a-z char word. - note: 16 chars or less - Added initial support for tmux is screen fails to detect - Prints out some macos/ubuntu installation text, with a 4 second sleep before starting 1mb minecraft script - This is basically how i want this, didn't test it on ubuntu but it should work. Tested it on macOS Catalina and will soon test and then push to network

--- Resources/Server/1MB-start.sh
+++ Resources/Server/1MB-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # @Filename: 1MB-start.sh
-# @Version: 2.0, build 027 for Spigot 1.16.1 (java 11)
+# @Version: 2.0, build 028 for Spigot 1.16.1 (java 11)
 # @Release: August 2nd, 2020
-# @Description: Helps us start and fork a Minecraft Spigot 1.16.1 server.
-# @Contact:	I am @floris on Twitter, and mrfloris in MineCraft.
+# @Description: Helps us start and fork a Minecraft 1.16.1 server.
+# @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: floris#0233 on https://discord.gg/KzTDhxv
 # @Install: chmod a+x 1MB-start.sh
 # @Syntax: ./1MB-start.sh
@@ -12,48 +12,72 @@
 
 ### CONFIGURATION
 #
-# Stuff here you COULD change, but ONLY if you really really have to
+# Declarations here you can customize to your preferred setup.
+# Generally only if you actually have to. Check Wiki for details.
 #
 ###
 
-#todo take input for servername $1 to set to _serverName
 _serverName="mcserver"
-# this name makes it easier to spot in screen -ls
+# This name makes it easier to spot in 'screen -ls'
 # Keep this lowercase, short, simple, no weird characters
 
-_debug=true
-# set to false if you want as little visual output as possible.
+### FUNCTIONS AND CODE
+#
+# ! WE ARE DONE, STOP EDITING BEYOND THIS POINT !
+#
+###
 
-#######################################################
-#### !You're done, stop editing beyond this point! ####
-#######################################################
-
-[ "$EUID" -eq 0 ] && echo -e "\n***!!*** This script should not be run using sudo, or as the root user!\n" && exit 1
-Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
 _sibling="1MB-minecraft.sh"
+_debug=true # Set to false to minimize output.
+
+Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
 
 function _output {
-	_args="${*:1}"; _prefix="(Debug)"
-	[[ "$_debug" == true ]] && echo -e "\\n$Y$_prefix$R$C $_args $R"
+    case "$1" in
+    oops)
+        _args="${*:2}"; _prefix="(Script Halted!)";
+        echo -e "\\n$B$Y$_prefix$X $_args $R" >&2; exit 1
+    ;;
+    okay)
+        _args="${*:2}"; _prefix="(Info)";
+        echo -e "\\n$B$Y$_prefix$C $_args $R" >&2; exit 1
+    ;;
+    debug)
+        _args="${*:2}"; _prefix="(Debug)";
+        [[ "$_debug" == true ]] && echo -e "\\n$Y$_prefix$C $_args $R"
+    ;;
+    *)
+        _args="${*:1}"; _prefix="(Info)";
+        echo -e "\\n$_prefix $_args"
+    ;;
+    esac
 }
 
-if [ ! -x "$_sibling" ]; then
- 	_output "This '$0' script requires '$_sibling' to work and be executable. Correct the permissions, or download it from https://scripts.1moreblock.com/ "
- 	exit 1
+[ "$EUID" -eq 0 ] && _output oops "*!* This script should not be run using sudo, or as the root user!"
+[ ! -x "$_sibling" ] && _output oops "This '$0' script requires '$_sibling' to work and be executable. Correct the permissions, or download it from https://scripts.1moreblock.com/ "
+
+if [ -n "$1" ]; then
+    _input=$(echo "$1" | awk '{ print tolower($1) }'); _input=$(echo "${_input}" | awk '{print substr ($0, 0, 16)}');
+    [[ "$_input" =~ ^[a-z]+$ ]] && _serverName="$_input" || _output oops "Provided input is invalid! Input is for a unique '_serverName'. Do not use numbers, spaces or weird chars. Keep it short, and a-z characters only."
 fi
 
-_output "Attempting to start your Minecraft '$_serverName' server ... "
+_output debug "Attempting to start your Minecraft '$_serverName' server ... "
 
 if type "screen" >/dev/null 2>&1; then
-	_output "Found 'screen', attempting to fork session into background ..."
-	screen -dmS $_serverName bash "$_sibling"
-	[[ "$_debug" == true ]] && screen -ls
+    _output debug "Found 'screen', attempting to fork session into background ..."
+    screen -dmS $_serverName bash "$_sibling"
+    [[ "$_debug" == true ]] && screen -ls
+elif type "tmux" >/dev/null 2>&1; then
+    _output debug "Could not find 'screen', but found 'tmux', attempting to fork session into background ..."
+    tmux new -d -s "$_serverName" 2>/dev/null || return 1
+    tmux send-keys -t "$_serverName" "./$_sibling" ENTER
+    [[ "$_debug" == true ]] && tmus ls && _output debug "To re-attach: tmux attach -t $_serverName"
 else
-	_output "Oops, 'screen' not installed. On Ubuntu, try 'apt install screen' \\nCan't fork session, trying '$_sibling' without ..."
-	sleep 4
-	bash "$_sibling" || _output "Something is wrong, I could not start your server."
+    _output debug "Oops, 'screen', nor 'tmux' seems to be installed. Try installing either. \\n -> macOS: brew install tmux, Ubuntu: apt install screen \\n -> Can't fork session, trying '$_sibling' directly ..."
+    sleep 4
+    bash "$_sibling" || _output oops "Something is wrong, I could not start your server."
 fi
 
-_output "Done."
+_output debug "Done."
 
 #EOF (c)2011-2020 Floris Fiedeldij Dop

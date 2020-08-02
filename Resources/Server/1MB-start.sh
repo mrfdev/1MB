2b0c008b1ba44e902efcf7b9e71cee6e97117aec
mrfloris <mrfloris@gmail.com>
2020-08-02 07:36:48 +0200

    Small update to version 2 - small version bump - updated some wording here and there - condensed some of the code - Offers instructions on how to install screen now - Preparing to support tmux - slightly longer delay when screen isn't found, before starting (without forking) - This update was to make the v2 code more consistent between shell scripts

--- Resources/Server/1MB-start.sh
+++ Resources/Server/1MB-start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # @Filename: 1MB-start.sh
-# @Version: 2.0, build 026 for Spigot 1.16.1 (java 11)
-# @Release: August 1st, 2020
+# @Version: 2.0, build 027 for Spigot 1.16.1 (java 11)
+# @Release: August 2nd, 2020
 # @Description: Helps us start and fork a Minecraft Spigot 1.16.1 server.
 # @Contact:	I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: floris#0233 on https://discord.gg/KzTDhxv
 # @Install: chmod a+x 1MB-start.sh
 # @Syntax: ./1MB-start.sh
-# @URL: Latest source, info, & support: https://scripts.1moreblock.com/
+# @URL: Latest source, wiki, & support: https://scripts.1moreblock.com/
 
 ### CONFIGURATION
 #
@@ -33,8 +33,7 @@ Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
 _sibling="1MB-minecraft.sh"
 
 function _output {
-	_args="${*:1}"
-	_prefix="(Debug)"
+	_args="${*:1}"; _prefix="(Debug)"
 	[[ "$_debug" == true ]] && echo -e "\\n$Y$_prefix$R$C $_args $R"
 }
 
@@ -50,8 +49,8 @@ if type "screen" >/dev/null 2>&1; then
 	screen -dmS $_serverName bash "$_sibling"
 	[[ "$_debug" == true ]] && screen -ls
 else
-	_output "Oops, 'screen' not installed. Can't fork session, trying '$_sibling' without ..."
-	sleep 3
+	_output "Oops, 'screen' not installed. On Ubuntu, try 'apt install screen' \\nCan't fork session, trying '$_sibling' without ..."
+	sleep 4
 	bash "$_sibling" || _output "Something is wrong, I could not start your server."
 fi
 

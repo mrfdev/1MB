0d4337ad5c3b97585c778d3aa843e27b259d546e
mrfloris <mrfloris@gmail.com>
2020-08-02 12:54:22 +0200

    Bigger v2 cleanup - Small version bump - Replaced a few tabs I spotted to spaces - Implemented toggle for automatically accepting EULA without having to edit the eula.txt file - Rephrased introduction text - Moved variable declarations beyond 'you can edit this' portion, since it almost never has to get changed and we have a little bit of auto detection. - Re-ordered some of the 'you can edit this' portion declarations so they're more logical. - Condensed a bit more code - Cleaned up how it all looks a bit more - Removed most Commenting - Set the default memory to 4gb, because 1.16 likes ram - Set EULA default to false, i want you to agree, i dont' agree for you. - Set the default for _debug to false, we dont need to output anything unless stuff goes wrong. - Rephrased some of the explanation text - Added a check if you've declared the variable _javaBin. If we cannot actually find it, we will halt and complain about it - When eula is set to true, the correct startup flag will be added to _javaParams - I am finally sort of happy with this version. Using this on my live servers that aren't linked to the network. And updating internal shell scripts to use some of these improvements.

--- Resources/Server/1MB-minecraft.sh
+++ Resources/Server/1MB-minecraft.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
 # @Filename: 1MB-minecraft.sh
-# @Version: 2.0, build 027 for Spigot 1.16.1 (java 11)
+# @Version: 2.0, build 028 for Spigot 1.16.1 (java 11)
 # @Release: August 2nd, 2020
 # @Description: Helps us start a Minecraft Spigot 1.16.1 server.
-# @Contact:	I am @floris on Twitter, and mrfloris in MineCraft.
+# @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: floris#0233 on https://discord.gg/KzTDhxv
 # @Install: chmod a+x 1MB-minecraft.sh
 # @Syntax: ./1MB-minecraft.sh
 # @URL: Latest source, wiki, & support: https://scripts.1moreblock.com/
 
-#todo toggle for eula?
-
 ### CONFIGURATION
 #
-# Stuff here you COULD change, but ONLY if you really really have to
+# Declarations here you can customize to your preferred setup.
+# Generally only if you actually have to. Check Wiki for details.
 #
 ###
 
-# Which version are we running?
 _minecraftVersion="1.16.1"
+# Which version are we running?
+
+_minJavaVersion=11.0
+# 11.0 = java 11, can be used for Minecraft 1.13.x and up.
+# 1.8 = java 8, required for Minecraft 1.12.x and up.
+
+_javaMemory="-Xms4G -Xmx4G"
+# "" = uses the default
+# "-Xmx2G" = maximum memory allocation pool of memory for JVM.
+# "-Xms1G" = initial memory allocation pool of memory for JVM.
+# For Spigot / Paper servers we recommend -Xms10G -Xmx10G for 16GB systems.
+# More details here: https://stackoverflow.com/questions/14763079/
+
+# jvm startup parameters
+_javaParams="-Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true"
+# -Dfile.encoding=UTF-8 (UTF-8 characters will be saved properly in the log files, and should correctly display in the console.)
+# -Dapple.awt.UIElement=true (Helps on macOS to not show icon in cmd-tab)
+# -Dhttps.protocols=TLSv1 (Temporary fix for older discordsrv, you can ignore this one probably)
 
 # Override auto engine jar detection; only use this if you have issues
 _engine=""
@@ -28,45 +44,31 @@ _engine=""
 # "paper" assumes to look for paper-1.16.1.jar
 
 _engineParams=""
-# 	!! Use only if you know you need this !!
-#	Leave empty! (for every day running)
-# 	--forceUpgrade (one time convert chunks to new engine)
-# 	--eraseCache (removing caches, lightning fix)
+# Leave empty for every day running, only edit when you need this!
+# --forceUpgrade (One time converts world chunks to new engine version)
+# --eraseCache (Removes caches. Cached data is used to store the skylight, blocklight and biomes, alongside other stuff)
 
-#JAVA_VERSION="11.0"
-_minJavaVersion=11.0
-#	11.0 = java 11, can be used for Minecraft 1.13.x and up.
-#	1.8 = java 8, required for Minecraft 1.12.x and up.
+# By changing the setting below to true you are indicating your agreement to Mojang's EULA 
+# which is legally binding, and you should read it! https://account.mojang.com/documents/minecraft_eula
+_eula=false
+
+# leave "" if you want the 1.16.1 server-gui
+_noGui="--nogui"
+
+### FUNCTIONS AND CODE
+#
+# ! WE ARE DONE, STOP EDITING BEYOND THIS POINT !
+#
+###
 
 _javaBin=""
-# Leave empty for auto-discovery of java path, if 
-# this fails, you could hard code the path, as below
-# 08 (if you want to make spigot for 1.12.2 or 1.13.2)
+# Leave empty for auto-discovery of java path, and 
+# if this fails, you could hard code the path, as below:
 # _javaBin="/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/java"
-# 11 (if you want to make spigot for 1.13.2 - 1.16.1)
 # _javaBin="/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java"
 
-_javaMemory="-Xms4G -Xmx4G"
-#	"" = uses the default
-#	"-Xmx2G" = maximum memory allocation pool of memory for JVM.
-#	"-Xms1G" = initial memory allocation pool of memory for JVM.
-# For Spigot servers we recommend -Xms10G -Xmx10G for 16GB systems.
-# More details here: https://stackoverflow.com/questions/14763079/
-
-_javaParams="-Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true"
-# 	-Dhttps.protocols=TLSv1 (temp fix for discordsrv)
-#	-Dcom.mojang.eula.agree=true (Mojang EULA agreement)
-#	-Dfile.encoding=UTF-8 (ensure that all UTF-8 chars are being saved properly)
-#	-Dapple.awt.UIElement=true (helps on macOS to not show icon in cmd-tab)
-
-_noGui="--nogui" # leave "" if you want the 1.16.1 server-gui
-
-# Debug mode on or off?
+# Debug mode off or on? Default: false (true means it spits out progress)
 _debug=false
-# Default: true (that means it spits out progress))
-#######################################################
-#### !You're done, stop editing beyond this point! ####
-#######################################################
 
 function _output {
     case "$1" in
@@ -89,44 +91,39 @@ function _output {
     esac
 }
 
-#prerequisites
-
 [ "$EUID" -eq 0 ] && _output oops "*!* This script should not be run using sudo, or as the root user!"
 Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
 
+function version_gt() { test "$(printf '%s\n' "$@"|sort -V|head -n 1)" != "$1"; }
 function binExists() { type "$1">/dev/null 2>&1; }
-
 function binDetails() { 
     _cmd="$_"; _cmd="$_cmd";
     _cmdpath=$(command -V "$_cmd" | awk '{print $3}')
-	_cmdversion=$($_cmd -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    # _output debug "Trying to find $_cmd, possibly here: $_cmdpath, version: $_cmdversion"
+    _cmdversion=$($_cmd -version 2>&1 | awk -F '"' '/version/ {print $2}')
 }
-
-function version_gt() { test "$(printf '%s\n' "$@"|sort -V|head -n 1)" != "$1"; }
 
 if binExists "java"; then
     binDetails "$_"
     if version_gt "$_cmdversion" "$_minJavaVersion"; then
         if [ -z "$_javaBin" ]; then
-            # if _javaBin is empty, we want to auto discover, lets try
             _output debug "_javaBin is empty, trying to auto discover java .."
             if [ -z "$_cmdpath" ]; then
-                # if cmdpath is empty, we are in trouble, quit script
-                _output oops "Path to java bin was found empty, maybe set _javaBin"
+                _output oops "Path to java bin was found empty, maybe set _javaBin manually"
             else
-                # else cmdpath is not empty, we could use that path, set it
                 _output debug "Path to java ($_cmdversion) auto discovered: $_cmdpath"
                 _javaBin="$_cmdpath"
             fi
         else
-            # else _javaBin is not empty, we want to use this instead of cmdpath, set it
-            _output debug "Path to java was set in _javaBin, using this instead of auto discovery."
-            # todo CONFIRM IF _javaBin IS ACTUALLY THERE else oops out
+            # todo: Reconsider how to approach this, if _javaBin is set, check that. If that fails, try auto discovery.
+            if [[ -f "$_javaBin" ]]; then
+        		_output debug "Path to java was set in _javaBin, found it and trying to use this instead of auto discovery."
+    		else
+    			_output oops "Could not find $_javaBin, leave _javaBin empty for auto discovery or install java properly."
+    		fi
         fi
         _output debug "Installed $_cmd version $_cmdversion is newer than $_minJavaVersion (this is great)!"
     else
-        _output oops "Installed $_cmd version $_cmdversion is NOT newer \\n -> Please upgrade to he minimal required version: $_minJavaVersion "
+        _output oops "Installed $_cmd version $_cmdversion is NOT newer \\n -> Please upgrade to the minimal required version: $_minJavaVersion "
     fi
 else
     _output oops "$_ was not found, please install it for this operating system \\n -> https://www.digitalocean.com/community/tutorials?q=install+java"
@@ -134,35 +131,30 @@ fi
 
 # before we continue, let's figure out if we have spigot or paper
 if [ -n "$_engine" ]; then
-	# It's not empty, let's use what they have and try to find that jar.
-	_serverJar="$_engine-$_minecraftVersion.jar"
-	if [[ -f "$_serverJar" ]]; then
-		# we found it, set it and continue the script. todo: we can simplify this logic
-		_engineJar="$_serverJar"
-	else
-		# we did not find it, time to complain and bail out.
-		_output oops "Oops, we did not find $_serverJar, please check your configuration."
-	fi
+    # It's not empty, let's use what they have and try to find that jar.
+    _serverJar="$_engine-$_minecraftVersion.jar"
+    if [[ -f "$_serverJar" ]]; then
+        _engineJar="$_serverJar"
+    else
+        _output oops "Oops, we did not find $_serverJar, please check your configuration."
+    fi
 else
-	# it's empty - let's figure out if there's a spigot or paper jar
-	_serverJar="paper-$_minecraftVersion.jar"
-	if [[ -f "$_serverJar" ]]; then
-		# we found paper, set it and continue the script. 
-		_engineJar="$_serverJar"
-	else
-		# we did not find paper, but maybe there's a spigot jar
-		_serverJar="spigot-$_minecraftVersion.jar"
-		if [[ -f "$_serverJar" ]]; then
-			# found spigot, set it and continue the script.
-			_engineJar="$_serverJar"
-		else
-			# we did not find paper, and now also not spigot, time to bail out.
-			_output oops "Oops, we did not find a paper or spigot, please read a manual."
-		fi
-	fi
+    # it's empty - let's figure out if there's a paper (first) or spigot (fallback)
+    _serverJar="paper-$_minecraftVersion.jar"
+    if [[ -f "$_serverJar" ]]; then
+        _engineJar="$_serverJar"
+    else
+        _serverJar="spigot-$_minecraftVersion.jar"
+        if [[ -f "$_serverJar" ]]; then
+            _engineJar="$_serverJar"
+        else
+            _output oops "Oops, we did not find a paper or spigot, please read a manual."
+        fi
+    fi
 fi
 
-# Below is your java start up line for the server, edit accordingly.
+[[ "$_eula" == true ]] && _javaParams="${_javaParams} -Dcom.mojang.eula.agree=true"
+
 _startJVM="$_javaBin $_javaMemory $_javaParams -jar $_engineJar $_engineParams $_noGui"
 $_startJVM || _output oops "Failed to start the jvm for some reason."
 

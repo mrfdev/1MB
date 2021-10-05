e023592d458fdfc9dfa8988371adc6fa52ad1103
mrfloris <mrfloris@gmail.com>
2021-10-05 20:44:24 +0200

    - version bump * merged if conditionals * used the theme i usually use * made script more consistent with how i do the rest * moved configurables to the top * better defined what goes where * tries to find all procs when getting port from server.properties fails * output of text can be removed by setting _debug to false

--- Resources/Server/1MB-CheckPort.sh
+++ Resources/Server/1MB-CheckPort.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # @Filename: 1MB-CheckPort.sh
-# @Version: 1.1, build 006
-# @Release: September 16th, 2021
+# @Version: 1.2, build 007
+# @Release: October 5th, 2021
 # @Description: Spits out if proc is running on port or not.
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: floris#0233 on https://discord.gg/KzTDhxv
@@ -17,37 +17,79 @@
 #
 ###
 
-_file="server.properties"
-# We will check if the current directory for example has a Paper 1.17.1 server.properties file
+_file="server.properties" # default server properties file
+_port="25565" # default port
+_proc="java" # default process to check for
 
-if [[ -f "$_file" ]]; then
-	#todo result might be empty, might need to check against that.
-	_port=$(grep "^server-port=" $_file | awk -F= '{print $2}')
-else
-	_port="25565"
-	echo -e "Could not grep port from $_file, defaulting to $_port"
-fi
-_proc="java"
+### INTERNAL CONFIGURATION
+#
+# Configuration variables you should probably
+# leave alone, but can change if really needed.
+#
+###
 
+_check="$_proc" # default for when all checks fail (lets just print all listening procs) 
+_debug=true # default spit out any output?
 
-# function
-_netfind() {
-	if [ $# -eq 0 ]; then
-		lsof -iTCP -sTCP:LISTEN -n -P || echo -e Nope, found nothing listening
-	elif [ $# -eq 1 ]; then
-		lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color "$1" || echo -e Nope, found nothing listening
-	else
-		echo -e "Usage: _netfind [proc|port]"
-	fi
+### END OF CONFIGURATION
+#
+# Really stop configuring things
+# beyond this point. I mean it.
+#
+###
+
+function _output {
+    case "$1" in
+    oops)
+        _args="${*:2}"; _prefix="(Script Halted!)";
+        echo -e "\\n$B$Y$_prefix$X $_args $R" >&2; exit 1
+    ;;
+    okay)
+        _args="${*:2}"; _prefix="(Info)";
+        echo -e "\\n$B$Y$_prefix$C $_args $R" >&2; exit 1
+    ;;
+    debug)
+        _args="${*:2}"; _prefix="(Debug)";
+        [[ "$_debug" == true ]] && echo -e "\\n$Y$_prefix$C $_args $R"
+    ;;
+    *)
+        _args="${*:1}"; _prefix="(Info)";
+        echo -e "\\n$_prefix $_args"
+    ;;
+    esac
 }
 
-# use what we have, and output
+[ "$EUID" -eq 0 ] && _output oops "*!* This script should not be run using sudo, or as the root user!"
+Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
+
+_netfind() {
+    if [ $# -eq 0 ]; then
+        lsof -iTCP -sTCP:LISTEN -n -P || _output debug "Nope, found nothing listening"
+    elif [ $# -eq 1 ]; then
+        lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color "$1" || _output debug "Nope, found nothing listening"
+    else
+        _output oops "Something went wrong."
+    fi
+}
+
+
+# Check if we have _file, if so, if we can find _port and show _proc, otherwise try to list all _proc
 if [[ -f "$_file" ]]; then
-	echo -e "Found $_file, lets find the running proc on $_port:"
-	_netfind $_port
+    _checkFile=`grep "^server-port=" $_file`
+    if [ -z "$_checkFile" ]
+        then
+        _output debug "We found the '$_file' file, but it has no port defined (check the file!)"
+        _output debug "Defaulting to checking for all instances of '$_proc' instead:"
+    else
+        _port=$(grep "^server-port=" $_file | awk -F= '{print $2}')
+        _output debug "We found the '$_file' file with a defined port \n lets find the running proc '$_proc' on port '$_port':"
+        _check="$_port"
+    fi
 else
-	echo -e "Did not find $_file, checking for all $_proc instead:"
-	_netfind $_proc
+    _output debug "Could not find the '$_file' file, checking for all instances of '$_proc' instead:"
 fi
+
+# And finally do the check
+_netfind $_check
 
 #EOF Copyright (c) 2011-2021 - Floris Fiedeldij Dop - https://scripts.1moreblock.com

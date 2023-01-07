1d31595df471c828afb5498e9b44854aa9cde3ac
Floris <mrfloris@gmail.com>
2023-01-07 03:02:46 +0100

    Update 1MB-template.sh
    - bump to 1.0.1;
    * added theme
    * added function for outputs
    * option to turn off debug, so there's no output (for crontab use, etc)
    * complaints now when run as root

--- Resources/Server/1MB-template.sh
+++ Resources/Server/1MB-template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-template.sh
-# @Version: 1.0.0, build 002
+# @Version: 1.0.1, build 003
 # @Release: January 7th, 2023
 # @Description: Helps us clone /template to /server
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
@@ -10,25 +10,53 @@
 # @Syntax: ./1MB-template.sh
 # @URL: Latest source, wiki, & support: https://scripts.1moreblock.com/
 
-# get todays date
+_debug=true # Set to false to minimize output.
+
+Y="\\033[33m"; C="\\033[36m"; R="\\033[0m" # theme
+
+function _output {
+    case "$1" in
+    oops)
+        _args="${*:2}"; _prefix="(Script Halted!)";
+        echo -e "\\n$B$Y$_prefix$X $_args $R" >&2; exit 1
+    ;;
+    okay)
+        _args="${*:2}"; _prefix="(Info)";
+        echo -e "\\n$B$Y$_prefix$C $_args $R" >&2; exit 1
+    ;;
+    debug)
+        _args="${*:2}"; _prefix="(Debug)";
+        [[ "$_debug" == true ]] && echo -e "$Y$_prefix$C $_args $R"
+    ;;
+    *)
+        _args="${*:1}"; _prefix="(Info)";
+        echo -e "\\n$_prefix $_args"
+    ;;
+    esac
+}
+
+_output debug "Okay, starting script.."
+
+[ "$EUID" -eq 0 ] && _output oops "*!* This script should not be run using sudo, or as the root user!"
+
+_output debug "Setting unique timestamp.."
 now=$(date +"%m_%d_%Y_%H%M%S")
 
-# change to working dir
+_output debug "Changing to working dir.."
 cd /home/minecraft
 
-# tar/gzip the server/ dir before we archive it, and restore from template/
+_output debug "Creating server-$now.tar.gz, this might take a moment.."
 tar -cpzf /home/minecraft/archive/server-$now.tar.gz -C /home/minecraft server
 
-# remove the old server
+_output debug "Purging old /server/ dir.."
 rm -rf /home/minecraft/server/
 
-# clone the template to server/
+_output debug "Restoring /server/ from /templates/.."
 cp -R /home/minecraft/templates/server/ /home/minecraft/server/
 
-# change to server dir, so we can start things
+_output debug "Changing to /server/ dir.."
 cd /home/minecraft/server/
 
-# we're done
-echo "Done. You can start the server with ./1MB-start.sh, it will fork to the background."
+_output okay "Done. You can start the server with ./1MB-start.sh, it will fork to the background."
 
 #EOF Copyright (c) 2011-2023 - Floris Fiedeldij Dop - https://scripts.1moreblock.com

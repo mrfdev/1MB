7c3f752c247bae63c7c3ea2be18b461c27f22bc0
mrfloris <mrfloris@gmail.com>
2023-06-19 01:36:42 +0200

    - modern shebang - draft for header - version bump to 0.0.2, build 002
    * .. to match my other shell scripts

--- Resources/BentoBox/1MB-BentoBox-Complete-Challenges-Fix.sh
+++ Resources/BentoBox/1MB-BentoBox-Complete-Challenges-Fix.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
-# ^ old fashioned shebang
+#!/usr/bin/env bash
 
-# header for 1MB-BentoBox-Complete-Challenges-Fix.sh
-# version 0.0.1, build 001
+# @Filename: 1MB-BentoBox-Complete-Challenges-Fix.sh
+# @Version: 0.0.2, build 002 for BentoBox+Challenges, on Minecraft 1.20.x
+# @Release: June 19th, 2023
+# @Description: Helps me re-sync completed challenges for a player.
+# @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
+# @Discord: @mrfloris on https://discord.gg/floris
+# @Install: chmod a+x 1MB-BentoBox-Complete-Challenges-Fix.sh
+# @Syntax: ./1MB-BentoBox-Complete-Challenges-Fix.sh
+# @URL: Latest source, wiki, & support: https://scripts.1moreblock.com/
+
+## TODO ##
+## > take script param for uuid, so we don't have to edit the script (.sh <uuid>)
 
 # configurable things
 

df2665638fa77bf727595082da8517b493d9e60d
mrfloris <mrfloris@gmail.com>
2023-06-19 01:31:49 +0200

    = initial draft of document * shebang * header, footer * config, non config blocks * version 0.0.1, build 001

---
+++ Resources/BentoBox/1MB-BentoBox-Complete-Challenges-Fix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ^ old fashioned shebang
+
+# header for 1MB-BentoBox-Complete-Challenges-Fix.sh
+# version 0.0.1, build 001
+
+# configurable things
+
+# not configurable things
+
+# footer

a3985e6d2c83b1f35757d5177defaa5ada1ae656
mrfloris <mrfloris@gmail.com>
2023-06-19 01:38:47 +0200

    - added TODO section for now - added todo items so i dont forget - added footer to match my other scripts

--- Resources/BentoBox/1MB-BentoBox-Complete-Challenges-Fix.sh
+++ Resources/BentoBox/1MB-BentoBox-Complete-Challenges-Fix.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-BentoBox-Complete-Challenges-Fix.sh
-# @Version: 0.0.2, build 002 for BentoBox+Challenges, on Minecraft 1.20.x
+# @Version: 0.0.2, build 003 for BentoBox+Challenges, on Minecraft 1.20.x
 # @Release: June 19th, 2023
 # @Description: Helps me re-sync completed challenges for a player.
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
@@ -12,9 +12,10 @@
 
 ## TODO ##
 ## > take script param for uuid, so we don't have to edit the script (.sh <uuid>)
+## > check if the .log file exists, halt and request to start fresh or append.
 
 # configurable things
 
 # not configurable things
 
-# footer
+#EOF Copyright (c) 2011-2023 - Floris Fiedeldij Dop - https://scripts.1moreblock.com

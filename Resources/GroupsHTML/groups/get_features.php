2383644cef8f58a1f5d725bc005ec286be436499
Floris Fiedeldij Dop <mrfloris@gmail.com>
2023-12-12 10:25:52 +0100

    - added placeholders for 25 groups, - todo, will have to figure out how many we will actually have in 2024+

--- Resources/GroupsHTML/groups/get_features.php
+++ Resources/GroupsHTML/groups/get_features.php
@@ -9,9 +9,101 @@ $playerGroups = array(
     "group2" => array(
         "Feature 1" => "Value 4",
         "Feature 2" => "Value 5",
+    ),
+    "group3" => array(
+        "Feature 1" => "Value 7",
+        "Feature 2" => "Value 8",
+    ),
+    "group4" => array(
+        "Feature 1" => "Value 10",
+        "Feature 2" => "Value 11",
+    ),
+    "group5" => array(
+        "Feature 1" => "Value 13",
+        "Feature 2" => "Value 14",
+    ),
+    "group6" => array(
+        "Feature 1" => "Value 16",
+        "Feature 2" => "Value 17",
+    ),
+    "group7" => array(
+        "Feature 1" => "Value 19",
+        "Feature 2" => "Value 20",
+    ),
+    "group8" => array(
+        "Feature 1" => "Value 22",
+        "Feature 2" => "Value 23",
+    ),
+    "group9" => array(
+        "Feature 1" => "Value 25",
+        "Feature 2" => "Value 26",
+    ),
+    "group10" => array(
+        "Feature 1" => "Value 28",
+        "Feature 2" => "Value 29",
+    ),
+    "group11" => array(
+        "Feature 1" => "Value 31",
+        "Feature 2" => "Value 32",
+    ),
+    "group12" => array(
+        "Feature 1" => "Value 34",
+        "Feature 2" => "Value 35",
+    ),
+    "group13" => array(
+        "Feature 1" => "Value 37",
+        "Feature 2" => "Value 38",
+    ),
+    "group14" => array(
+        "Feature 1" => "Value 40",
+        "Feature 2" => "Value 41",
+    ),
+    "group15" => array(
+        "Feature 1" => "Value 43",
+        "Feature 2" => "Value 44",
+    ),
+    "group16" => array(
+        "Feature 1" => "Value 46",
+        "Feature 2" => "Value 47",
+    ),
+    "group17" => array(
+        "Feature 1" => "Value 49",
+        "Feature 2" => "Value 50",
+    ),
+    "group18" => array(
+        "Feature 1" => "Value 52",
+        "Feature 2" => "Value 53",
+    ),
+    "group19" => array(
+        "Feature 1" => "Value 55",
+        "Feature 2" => "Value 56",
+    ),
+    "group20" => array(
+        "Feature 1" => "Value 58",
+        "Feature 2" => "Value 59",
+    ),
+    "group21" => array(
+        "Feature 1" => "Value 61",
+        "Feature 2" => "Value 62",
+    ),
+    "group22" => array(
+        "Feature 1" => "Value 64",
+        "Feature 2" => "Value 65",
+    ),
+    "group23" => array(
+        "Feature 1" => "Value 67",
+        "Feature 2" => "Value 68",
+    ),
+    "group24" => array(
+        "Feature 1" => "Value 70",
+        "Feature 2" => "Value 71",
+    ),
+    "group25" => array(
+        "Feature 1" => "Value 73",
+        "Feature 2" => "Value 74",
     )
-    // 2024 groups are like 20, r&repeat
 );
+
 
 // Which group is sent via AJAX POST request
 if (isset($_POST['group'])) {

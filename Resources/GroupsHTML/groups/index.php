71914459faa8f20c52f31a57f37c5f680f2a04c0
Floris Fiedeldij Dop <mrfloris@gmail.com>
2023-12-12 09:37:49 +0100

    - quick html5 template to build upon

---
+++ Resources/GroupsHTML/groups/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>groups</title>
+  <!-- resources section -->
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <header>
+    <!-- header section -->
+    <h1>groups</h1>
+  </header>
+
+  <main>
+    <!-- main content section -->
+    <p>groups table and selector</p>
+  </main>
+
+  <footer>
+    <!-- footer section -->
+    <p>&copy; <?php echo date("Y"); ?> 1MoreBlock.com - Floris Fiedeldij Dop</p>
+  </footer>
+
+  <!-- scripts section -->
+  <script src="script.js"></script>
+</body>
+</html>

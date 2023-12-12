875204c3c717feb57da993153590fa6e135b253a
Floris Fiedeldij Dop <mrfloris@gmail.com>
2023-12-12 09:57:12 +0100

    - decided to switch over to a global container to make flex/jQuery life easier.

--- Resources/GroupsHTML/groups/index.php
+++ Resources/GroupsHTML/groups/index.php
@@ -7,14 +7,16 @@
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <header>
-    <!-- header section -->
-    <h1>Player Groups - 1MoreBlock.com</h1>
-  </header>
+  <div class="container">
 
-  <main>
-    <!-- main content section -->
-    <p>groups table and selector</p>
+    <header>
+      <!-- header section -->
+      <h1>Player Groups - 1MoreBlock.com</h1>
+    </header>
+
+    <main>
+      <!-- main content section -->
+      <p>groups table and selector</p>
 
     <label for="groupSelect">Select a Player Group:</label>
     <select id="groupSelect">
@@ -27,15 +29,16 @@
       <!-- AJAX content will be loaded here -->
     </div>
 
-  </main>
+    </main>
 
-  <footer>
-    <!-- footer section -->
-    <p>Player Groups &copy; <?php echo date("Y"); ?> 1MoreBlock.com - Floris Fiedeldij Dop</p>
-  </footer>
+    <footer>
+      <!-- footer section -->
+      <p>Player Groups &copy; <?php echo date("Y"); ?> 1MoreBlock.com - Floris Fiedeldij Dop</p>
+    </footer>
 
-  <!-- scripts section -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  </div>
 
+    <!-- scripts section -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
 </html>

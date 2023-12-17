852ad89aed584f24363bd528d23bd4e71de10681
Floris Fiedeldij Dop <mrfloris@gmail.com>
2023-12-17 19:00:01 +0100

    - config part, internal config part. - added the usual theme - toggle for debug mode

--- Resources/ChristmasPlus/1MB-ChristmasPlus.sh
+++ Resources/ChristmasPlus/1MB-ChristmasPlus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # @Filename: 1MB-ChristmasPlus.sh
-# @Version: 0.0.1, build 001
+# @Version: 0.0.1, build 002
 # @Release: December 18th, 2023
 # @Description: Helps us get some player data from ChristmasPlus database.db
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
@@ -28,4 +28,22 @@ _userName="FumbleHead"
 # output to a log file?
 _log="false"
 _logFile="christmasplus-results-$_userName-.log"
+
+### INTERNAL CONFIGURATION
+#
+# Configuration variables you should probably
+# leave alone, but can change if needed.
+#
+###
+
+_debug=true # Set to false to minimize output.
+
+Y="\e[33m"; C="\e[36m"; PB="\e[38;5;153m"; B="\e[1m" R="\e[0m" # theme
+
+### END OF CONFIGURATION
+#
+# Stop configuring things
+# beyond this point. I mean it.
+#
+###
 

8067eac5e7da0bfc9131c5d04c3af61607e90a89
Floris Fiedeldij Dop <mrfloris@gmail.com>
2023-12-19 19:24:21 +0100

    - finally had time to fix the header and footer to match the omg .com/vote site

--- Resources/GroupsHTML/groups/includes/footer.php
+++ Resources/GroupsHTML/groups/includes/footer.php
@@ -8,10 +8,12 @@ if (!defined('INCLUDED')) {
 
   <footer class="py-3 bg-dark">
   <!-- footer section -->
-    <p class="m-0 text-center text-white">Player Groups &copy; 1977-<?php echo date("Y"); ?> 1MoreBlock.com - Floris Fiedeldij Dop
-    <br>
-    <small>Note please that Floris, the team members, OMGboards.com nor 1MoreBlock.com, claim or pretend to be-, nor are associated with-, and are not supported by Mojang or Microsoft, Discord, or any other brandname. Server owner mrfloris (mrfloris@gmail.com)</small>
-    </p>
+    <div class="container navbar-dark bg-dark headerFooterFix">
+      <p class="m-0 text-center text-white">Player Groups &copy; 1977-<?php echo date("Y"); ?> 1MoreBlock.com - Floris Fiedeldij Dop
+      <br>
+      <small>Note please that Floris, the team members, OMGboards.com nor 1MoreBlock.com, claim or pretend to be-, nor are associated with-, and are not supported by Mojang or Microsoft, Discord, or any other brandname. Server owner mrfloris (mrfloris@gmail.com)</small>
+      </p>
+    </div>
   </footer>
 
   <!-- scripts section -->
--- Resources/GroupsHTML/groups/includes/header.php
+++ Resources/GroupsHTML/groups/includes/header.php
@@ -20,33 +20,35 @@ if (!defined('INCLUDED')) {
 
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="index.php">1MoreBlock.com</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">GROUPS
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://omgboards.com/vote/">Voting</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://omgboards.com/forums/minecraft/">Forums</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://omgboards.com/threads/how-to-get-started.260944/">Minecraft</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://discord.1moreblock.com/">Discord</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://omgboards.com/forums/1mbplayers/post-thread">Support</a>
-        </li>
-      </ul>
+    <div class="container navbar-dark bg-dark headerFooterFix">
+      <a class="navbar-brand" href="index.php">1MoreBlock.com</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">GROUPS
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://omgboards.com/vote/">Voting</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://omgboards.com/forums/minecraft/">Forums</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://omgboards.com/threads/how-to-get-started.260944/">Minecraft</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://discord.1moreblock.com/">Discord</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://omgboards.com/forums/1mbplayers/post-thread">Support</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 
@@ -54,7 +56,6 @@ if (!defined('INCLUDED')) {
 
     <header>
       <!-- header section -->
-      <h1>Player Groups - 1MoreBlock.com</h1>
-      <div>We have a four-tier group hierarchy: free survival, free builders, patron, and special.</div>
+      <h1><strong>Player Groups</strong> - 1MoreBlock.com</h1>
       <hr>
     </header>

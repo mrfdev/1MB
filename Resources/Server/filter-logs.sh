f401a9bd7b99a6c4c62daa164c89672c9b8950be
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-06-04 21:38:47 +0200

    - start up and log file

--- Resources/Server/filter-logs.sh
+++ Resources/Server/filter-logs.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <log_file> <username1> [<username2> ... <usernameN>]"
+  exit 1
+fi
+
+log_file=$1
+if [ ! -f "$log_file" ]; then
+  echo "Log file '$log_file' not found!"
+  exit 1
+fi

9119e93f79ab868eb31396723e96bb43157920d1
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-06-13 23:52:00 +0200

    - dump to 1.21 + general cleanup - removing mentions of old java and mc , i will never run that anymore anyway

--- Resources/Server/1MB-minecraft.sh
+++ Resources/Server/1MB-minecraft.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-minecraft.sh
-# @Version: 2.17.4, build 068 for Minecraft 1.20.6 (Java 22.0.1, 64bit)
-# @Release: April 30th, 2024
-# @Description: Helps us start a Spigot or Paper 1.20.6 server.
+# @Version: 2.18.0, build 069 for Minecraft 1.21 (Java 22.0.1, 64bit)
+# @Release: June 13th, 2024
+# @Description: Helps us start a Spigot or Paper 1.21 server.
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: @mrfloris on https://discord.gg/floris
 # @Install: chmod a+x 1MB-minecraft.sh
@@ -17,19 +17,15 @@
 #
 ###
 
-_minecraftVersion="1.20.6"
+_minecraftVersion="1.21"
 # Which version are we running?
 
-_minJavaVersion="21"
+_minJavaVersion=22
+# use 22 for java 22.0.1 which can be used with Minecraft 1.20.6 and 1.21
 # use 21 for java 21.0.2 or 22.0.1 which can be used with Minecraft 1.19.x and 1.20.6
-# use 20.0 for java 20.0.2 which can be used with Minecraft 1.19.x and 1.20.1
-# use 19.0 for java 19.0.2 which can be used with Minecraft 1.19.3 and 1.19.4
-# use 18.0 for java 18.0.2.1 which can be used with Minecraft 1.19.2 and up
-# use 17.0 for java 17.0.5 or newer which can be used for Minecraft 1.17.1 and up.
-# use 16.0 for java 16 which is required for Minecraft 1.17.1 and up.
-# use 16.0 for java 16 which can be used for Minecraft 1.16.5 and up.
-# use 11.0 for java 11 which can be used for Minecraft 1.13.x and up to 1.16.5
-# use 1.8 for java 8 which can be used for Minecraft 1.12.x and up to 1.16.5
+# use 20.0 for java 20.0.2 which can be used with Minecraft 1.19.x and 1.20+
+# use 18.0, 19.0 for java 18.0.2.1+ which can be used with Minecraft 1.19+
+# use 17.0 for java 17.0.5 which can be used for Minecraft 1.17.x
 
 _javaMemory="-Xms4G -Xmx4G"
 # "" = uses the default
@@ -57,9 +53,9 @@ _javaParams="-Dfile.encoding=UTF-8 -Dapple.awt.UIElement=true"
 # Override auto engine jar detection; only use this if you have issues
 _engine="spigot"
 # spigot until paper jar is out
-# "" assumes auto detection for <engine>-1.20.6.jar 
-# "spigot" assumes to look for spigot-1.20.6.jar
-# "paper" assumes to look for paper-1.20.6.jar
+# "" assumes auto detection for <engine>-1.21.jar 
+# "spigot" assumes to look for spigot-1.21.jar
+# "paper" assumes to look for paper-1.21.jar
 
 _engineParams=""
 # Leave empty for every day running, only edit when you need this!
@@ -90,9 +86,6 @@ _javaBin=""
 # _javaBin="/Library/Java/JavaVirtualMachines/jdk-19.0.2.jdk/Contents/Home/bin/java"
 # _javaBin="/Library/Java/JavaVirtualMachines/jdk-18.0.2.1.jdk/Contents/Home/bin/java"
 # _javaBin="/Library/Java/JavaVirtualMachines/jdk-17.0.5.jdk/Contents/Home/bin/java"
-# _javaBin="/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home/bin/java"
-# _javaBin="/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java"
-# _javaBin="/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/java"
 
 _debug=true
 # Debug mode off or on? Default: false (true means it spits out progress)

8506adebba534ebfe8df4a9a1ae5c1ff6166d809
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-08-26 14:55:38 +0200

    - bash starts, build 002 - define some variables - use those to make internal variables

--- Resources/Server/1MB-backup.sh
+++ Resources/Server/1MB-backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-backup.sh
-# @Version: 0.0.1, build 001 for Minecraft 1.21.1 (Java 22.0.2, 64bit)
+# @Version: 0.0.1, build 002 for Minecraft 1.21.1 (Java 22.0.2, 64bit)
 # @Release: August 26th, 2024
 # @Description: Helps us make a compressed tarball of a Minecraft 1.21.1 server. 
 # @Description: Note: Does not use rsync, this is meant for small servers only.
@@ -18,7 +18,8 @@
 #
 ###
 
-x 
+dir="MinecraftServer"
+backup_dir="_backups"
 
 ### INTERNAL CONFIGURATION
 #
@@ -27,7 +28,7 @@ x
 #
 ###
 
-x
+backup_file="${backup_dir}/${dir}-$(date +%d-%m-%Y).tar.gz"
 
 ### FUNCTIONS AND CODE
 #

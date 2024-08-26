668a8477ec87ed78bd4b371f935ee609af55bd7a
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-08-26 14:54:36 +0200

    - making a quick dirty shell bash script to help make compressed tarball snapshots of a minecraft server Note: I recommend subversioning and snapshotting properly. for example: rsync Note: This is meant for small servers that just need a quick tar.gz for easy rollbacks during project staging

---
+++ Resources/Server/1MB-backup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# @Filename: 1MB-backup.sh
+# @Version: 0.0.1, build 001 for Minecraft 1.21.1 (Java 22.0.2, 64bit)
+# @Release: August 26th, 2024
+# @Description: Helps us make a compressed tarball of a Minecraft 1.21.1 server. 
+# @Description: Note: Does not use rsync, this is meant for small servers only.
+# @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
+# @Discord: @mrfloris on https://discord.gg/floris
+# @Install: chmod a+x 1MB-backup.sh
+# @Syntax: ./1MB-backup.sh
+# @URL: Latest source, wiki, & support: https://scripts.1moreblock.com/
+
+### CONFIGURATION
+#
+# Declarations here you can customize to your preferred setup.
+# Generally only if you actually have to. Check Wiki for details.
+#
+###
+
+x 
+
+### INTERNAL CONFIGURATION
+#
+# Configuration variables you should probably
+# leave alone, but can change if really needed.
+#
+###
+
+x
+
+### FUNCTIONS AND CODE
+#
+# ! WE ARE DONE, STOP EDITING BEYOND THIS POINT !
+#
+###
+
+x
+
+#EOF Copyright (c) 2011-2024 - Floris Fiedeldij Dop - https://scripts.1moreblock.com

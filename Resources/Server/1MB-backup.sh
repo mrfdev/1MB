5fdbe6197c8bfc682f9d85cc8489fba85faf4a5c
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-08-26 14:58:42 +0200

    - we need to check if the variables set are correct - do these directories exist, if not - exit script, or create them if needed

--- Resources/Server/1MB-backup.sh
+++ Resources/Server/1MB-backup.sh
@@ -36,6 +36,19 @@ backup_file="${backup_dir}/${dir}-$(date +%d-%m-%Y).tar.gz"
 #
 ###
 
-x
+# Check if the desired directories exist, if not, exit script and report back.
+
+# Note, this is the directory from within the server(s) run, it SHOULD exist, we will not create it if it doesn't: properly configure the path if it fails
+if [ ! -d "$dir" ]; then
+    echo "Error: Directory $dir does not exist."
+    exit 1
+fi
+
+# However, if the backup dir does not exist, we can create it. Sometimes we purge the whole dir after moving it off-site.
+if [ ! -d "$backup_dir" ]; then
+    echo "Warning: $backup_dir directory does not seem to exist. Creating it now..."
+    mkdir -p "$backup_dir"
+fi
+
 
 #EOF Copyright (c) 2011-2024 - Floris Fiedeldij Dop - https://scripts.1moreblock.com

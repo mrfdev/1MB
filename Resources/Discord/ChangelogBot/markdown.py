bb5d68265467946543d041b9707c3f087fa8dcb9
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-11-02 04:22:22 +0100

    fix print

--- Resources/Discord/ChangelogBot/markdown.py
+++ Resources/Discord/ChangelogBot/markdown.py
@@ -1,7 +1,7 @@
 # markdown.py
 # 1MoreBlock.com Discord Script to convert messages stored with changelog.py (json format) to markdown so we can use it on discord and xenforo forums.
 # We are not using discord.py but Disnake to poke the Discord API.
-# Build 002, https://github.com/mrfdev/1MB/tree/master/Resources/Discord/ChangelogBot
+# Build 003, https://github.com/mrfdev/1MB/tree/master/Resources/Discord/ChangelogBot
 
 # Imports
 
@@ -27,4 +27,4 @@ with open(OUTPUT_MD_FILENAME, "w") as md_file:
 
 # And we're done. We're not a bot, no need for tokens and i dunno what here. 
 # Just print out we're donesies
-print("Converted {OUTPUT_JSON_FILENAME} file to {OUTPUT_MD_FILENAME}") #bye
+print(f"Converted {OUTPUT_JSON_FILENAME} file to {OUTPUT_MD_FILENAME}") #bye

ba3451ce77668b1a2b6303162b93693b3e46fc5a
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-11-02 01:59:30 +0100

    - added tokens to constants section - googled for intent setup, and set the prefix ! - thought even harder about the bot event - added sleep to batch handling

--- Resources/Discord/ChangelogBot/changelog.py
+++ Resources/Discord/ChangelogBot/changelog.py
@@ -1,7 +1,7 @@
 # changelog.py
 # 1MoreBlock.com Discord Bot Script to fetch messages from a internal changelog channel to save results to a JSON database file.
 # We are not using discord.py but Disnake to poke the Discord API.
-# Build 002, https://github.com/mrfdev/1MB/tree/master/Resources/Discord/ChangelogBot
+# Build 003, https://github.com/mrfdev/1MB/tree/master/Resources/Discord/ChangelogBot
 
 # Imports
 
@@ -16,24 +16,32 @@ OUTPUT_JSON_FILENAME = "changelog-msgs.json"
 
 # Discord related Tokens
 
-#discordtoken
-#channelid
+TOKEN = 'BOTTOKEN'  # lets not put our secrets here, shh
+CHANNEL_ID = 1059074047280939078  # our actual internal changelog channel id, no real secrets there
 
 # init msgs intent
+# According to google example bots, this is what I need. 
+intents = disnake.Intents.default()
+intents.message_content = True  # This Enables the msg content intent access, whatever thing that's now required to have enabled.
 
-# build a bot instance
-
-#codetobuildbotinstance
+# build a bot instance, i like !
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # async botevent
 
 @bot.event
 async def on_ready():
 	# let us know we did it
-	# init stuff
+	# {bot.user}
+	print(f"Okay, {bot.user} reporting in..")
+
+	# init stuff, i assume i need channel id, messages from the channel, and start a loop with last id number since we're batching it up
+	channel = something[CHANNEL_ID] # <- figure out synopsis
+	messages = [] # <- init array for the fetched msgs
+	last_message_id = None # <- Let's start empty, append later after each batch
 
 	# loop here, probably a while loop
-	# while true, try this .. 
+	# while true, try this .. except that..
 	# dont forgeto to try and batch fetch msgs
 	# then break if we got nothing to fetch
 
@@ -43,6 +51,34 @@ async def on_ready():
 	# save results to json file, python uses write or open() as file, ill have to google again
 
 	# async sleep() for a while, seconds or tens of seconds?
+	await asyncio.sleep(5)
+
+	### temp
+			# while True ..
+				# try:
+					# bunch of stuff
+					# batch = whatever
+						# if not batch, then break
+
+					# for each msg in msgs:
+						# msg.append etc ...
+							# key 1
+							# key 2
+							# key 3
+					# end for something
+
+					# open file and append to json
+						# json.dump(whatever) # do i need to think about indentation?
+
+					# update last.msg.id=[from batch result]
+
+					# sleep async 5s
+
+				# except HTTP exception error (google this)
+					# print(f"rate limit hit, pause & try again")
+					# await asyncio.sleep(try.again.after x seconds)
+
+				# and that's it
 
 # we're done, spit out that we got nothing to do
 print("All messages fetched from discord and saved to {OUTPUT_JSON_FILENAME}.")

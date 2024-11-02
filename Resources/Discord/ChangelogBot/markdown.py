e364d3bdc189c788921f88d50d6f49613b89bf17
Floris Fiedeldij Dop <mrfloris@gmail.com>
2024-11-02 02:54:38 +0100

    - make a working while loopy loopz - wrote the array results to the markdown .md file

--- Resources/Discord/ChangelogBot/markdown.py
+++ Resources/Discord/ChangelogBot/markdown.py
@@ -1,7 +1,7 @@
 # markdown.py
 # 1MoreBlock.com Discord Script to convert messages stored with changelog.py (json format) to markdown so we can use it on discord and xenforo forums.
 # We are not using discord.py but Disnake to poke the Discord API.
-# Build 001, https://github.com/mrfdev/1MB/tree/master/Resources/Discord/ChangelogBot
+# Build 002, https://github.com/mrfdev/1MB/tree/master/Resources/Discord/ChangelogBot
 
 # Imports
 
@@ -20,13 +20,10 @@ with open(OUTPUT_JSON_FILENAME, "r") as file: # open it!
 # We have the data, time to convert.
 
 with open(OUTPUT_MD_FILENAME, "w") as md_file:
-	# no idea about format yet
-	# md_file.write("content\n")
-	# this will be {message['key']}
-	# key will be author, content, timestamp
-
-	# for msg in msgs loop probably, so we can go and append to changelog-msgs.md
-	md_file.write(f"just testing")
+    md_file.write("# Changelog 2024\n\n")  # main heading
+    for message in messages:
+        md_file.write(f"### {message['timestamp']}\n")  # Write a timestamp heading for each message
+        md_file.write(f"**{message['author']}**: {message['content']}\n\n")  # Write the author's name and content
 
 # And we're done. We're not a bot, no need for tokens and i dunno what here. 
 # Just print out we're donesies

7e4935f8514ebadd6b51aff29b05a4a638e8e656
Floris Fiedeldij Dop <mrfloris@gmail.com>
2025-04-12 22:10:03 +0200

    - build bump - took cli params into consideration - prepped for code logic - is this a blacklisted string - helper prepped - updated temp stuff in anticipation of if conditionals

--- Resources/Server/1MB-parse-logins.sh
+++ Resources/Server/1MB-parse-logins.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-parse-logins.sh
-# @Version: 0.0.1, build 001
+# @Version: 0.0.3, build 003
 # @Release: April 12th, 2025
 # @Description: Helps us find alt accounts from /logs/
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: @mrfloris on https://discord.gg/floris
 # @Install: chmod +x 1MB-parse-logins.sh
 # @Syntax: ./1MB-parse-logins.sh
-# TODO:  <playername | ip | uuid> params?
+# ./parse-logins.sh Normal mode, generate log and IP sharing report
+# ./parse-logins.sh --search-user Notch
+# ./parse-logins.sh --search-ip 127.0.0.1
+# ./parse-logins.sh --search-uuid 069a79f4-44e9-4726-a5be-fca90e38aaf5
+
 # @URL: Latest source, wiki, & support: https://scripts.1moreblock.com/
 
 # @info:
@@ -19,10 +23,10 @@
 # - Generates a second file "2025-04-12-completed.log" showing which usernames share the same IP
 # - Blacklist for <playername> and <ip> to filter out knowns/falsepositives
 
-### CONFIGURATION
+### YOUR CONFIGURATION
 #
 # Declarations here you can customize to your preferred setup.
-# Generally only if you actually have to. Check Wiki for details.
+# Generally only if you actually have to. 
 #
 ###
 
@@ -35,20 +39,32 @@ BLACKLIST_USERS="./blacklist_users.txt"
 BLACKLIST_IPS="./blacklist_ips.txt"
 BLACKLIST_UUIDS="./blacklist_uuids.txt"
 
+### OPTIONAL INTERNAL CONFIGURATION
+#
+# Declarations here that you don't have to think about
+#
+###
+
 SEARCH_USER=""
 SEARCH_IP=""
 SEARCH_UUID=""
 
-# TODO
+#### TODO
+
 # - create blacklist_users.txt looking like
 # Notch
 # mrfloris
+
 # - create blacklist_ips.txt looking like
 # 127.0.0.1
 # 192.168.0.1
+
 # - create blacklist_uuids.txt looking like
-# 39ffc683-669a-4c5f-9e27-7f3fce3cc4e1
-# 29ffc683-669a-4c5f-9e27-7f3fce3cc4e2
+# 069a79f4-44e9-4726-a5be-fca90e38aaf5
+# 631e3896-da2a-4077-974b-d047859d76bc
+
+# echo the output so we know we've started
+# echo the output so we know we're done
 
 
 ### FUNCTIONS AND CODE
@@ -57,18 +73,50 @@ SEARCH_UUID=""
 #
 ###
 
-# Ensure output files are empty
-> "$RAW_LOG"
-> "$FINAL_LOG"
-# might need to figure this out later for automated crontabbing
+# Handy helper that we can use later
+is_blacklisted() {
+  local item="$1"
+  local file="$2"
+  grep -q -x "$item" "$file" 2>/dev/null
+}
 
-# TODO
-# echo the output so we know we've started
-# Find and process log files (plain and gzipped)
-# Check blacklists
-# Sort and deduplicate
-# Group usernames by IP with awk?
-# echo the output so we know we're done
+# Before we make some temp files, let's identify what type of search we're trying to do
+# Are we querying for a playername, playerip, or playeruuid, else report the param syntax is invalid, else skip this whole thing and do everything.
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --search-user)
+      SEARCH_USER="$2"
+      shift 2
+      ;;
+    --search-ip)
+      SEARCH_IP="$2"
+      shift 2
+      ;;
+    --search-uuid)
+      SEARCH_UUID="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Usage: $0 [--search-user USERNAME] [--search-ip IP] [--search-uuid UUID]"
+      exit 1
+      ;;
+  esac
+done
+
+# Okay, now we need some temp files
+TMP_LOG=$(mktemp)
+UUID_LOG=$(mktemp)
+
+# Parse the log files, so we have something to work with
+
+# Do the particular query type
+
+# Then sort the results, regardless of type!
+
+# The magic awk magic goes here probably.. gosh, i hope i can figure that out
+
+# Output results
 
 
 #EOF Copyright (c) 1977-2025 - Floris Fiedeldij Dop - https://scripts.1moreblock.com

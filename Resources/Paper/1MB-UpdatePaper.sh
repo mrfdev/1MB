f41bfaa3b3510f8a20f2f469eec72de062722f68
Floris Fiedeldij Dop <mrfloris@gmail.com>
2025-04-12 21:24:03 +0200

    - Switch to experimental channel for the time being - Trying to get 1.21.5 - Note: do not use for production; missing data converter stuff

--- Resources/Paper/1MB-UpdatePaper.sh
+++ Resources/Paper/1MB-UpdatePaper.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # @Filename: 1MB-UpdatePaper.sh
-# @Version: 3.2.15, build 041
-# @Release: February 2nd, 2025
-# @Description: Helps us get a Minecraft Paper 1.21.4 server .jar
+# @Version: 3.2.16, build 042
+# @Release: April 12th, 2025
+# @Description: Helps us get a Minecraft Paper 1.21.5 server .jar
 # @Contact: I am @floris on Twitter, and mrfloris in MineCraft.
 # @Discord: @mrfloris on https://discord.gg/floris
 # @Install: chmod +x 1MB-UpdatePaper.sh
@@ -29,7 +29,7 @@ _saveDir="."
 # Hard set the projects-controller to check for, we're going for paper
 # There's currently no support for other projects.
 _apiProject="paper"
-_apiChannel="default"
+_apiChannel="experimental"
 _apiURL="https://api.papermc.io/v2/projects"
 
 ### INTERNAL CONFIGURATION
